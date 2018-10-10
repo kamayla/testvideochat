0483cdd2ab1733f815ae164b33c53e925fc3d507
Ippei Kamimura <ippei_kamimura@icloud.com>
2018-10-10 18:11:36 +0700

    first commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,36 @@
+<?php
+namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use \Pusher\Pusher;
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+    public function authenticate(Request $request) {
+        $socketId = $request->socket_id;
+        $channelName = $request->channel_name;
+        $pusher = new Pusher(env('PUSHER_APP_KEY'), env('PUSHER_APP_SECRET'), env('PUSHER_APP_ID'), [
+            'cluster' => 'ap2',
+            'encrypted' => true
+        ]);
+        $presence_data = ['name' => auth()->user()->name];
+        $key = $pusher->presence_auth($channelName, $socketId, auth()->id(), $presence_data);
+        return response($key);
+    }
+}
